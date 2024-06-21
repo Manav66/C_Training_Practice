44dc27e1c67a468a8b34c28c7e39fb4deb08cdb4
Manav66 <manavverma51@gmail.com>
2024-06-21 06:35:48 +0000

    Size Files

---
+++ headerfile.c
@@ -0,0 +1,14 @@
+// Header Files
+#include<stdio.h>
+#include<string.h>
+
+// Macro
+
+#define space printf("\n")
+
+// Macro functions
+
+char strfunc(char ch[]){
+     printf("%s\n", ch);
+        }
+
